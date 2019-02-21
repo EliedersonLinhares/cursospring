eeef08751106032d7ef9275230a82c9abd95d090
Eliederson da Silva Linhares <eliederson250@gmail.com>
2019-02-21 10:05:08 -0300

    Baco de Dados H2

--- src/main/java/com/esl/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/esl/cursospring/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.esl.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.esl.cursospring.domain.Categoria;
 
 @RestController //Anotação que determina que a classe vai ser um controlador rest
 @RequestMapping(value="/categorias") // respondendo pelo endpoint "/categorias
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET) // http para obter dados
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
