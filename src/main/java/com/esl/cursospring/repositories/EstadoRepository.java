0dde30a29c6ad3b0dcacbd74c7a8ca3417982e9e
Eliederson da Silva Linhares <eliederson250@gmail.com>
2019-02-26 11:24:01 -0300

    Tabelas Estado e Cidade

---
+++ src/main/java/com/esl/cursospring/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.esl.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.esl.cursospring.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {//<Tipo, Atributo identificador(do "id" da classe categoria)>
+ //JpaRepositry -> tipo especial do spring capaz de acessar os dados com base no tipo passado
+}
