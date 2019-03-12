015b3eb90cf3a472ca9f7afa50862c6917acf755
Eliederson da Silva Linhares <eliederson250@gmail.com>
2019-03-12 15:58:33 -0300

    validação de email na inserção de cliente

--- src/main/java/com/esl/cursospring/repositories/ClienteRepository.java
+++ src/main/java/com/esl/cursospring/repositories/ClienteRepository.java
@@ -2,10 +2,24 @@ package com.esl.cursospring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.esl.cursospring.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {//<Tipo, Atributo identificador(do "id" da classe categoria)>
  //JpaRepositry -> tipo especial do spring capaz de acessar os dados com base no tipo passado
+	
+	
+	/*
+	 * busca por email
+	 * Escrevendo o nome do metodo com as letras findByEmail o Spring automaticamente
+	 * vai detectar que você quer fazer uma busca por email e irá implementar o 
+	 * método.
+	 * @Transactional(readOnly=true) -> não necessita ser envolvida como uma transação
+	 * de banco de dados, fazendo essa operação ficar mais rápida e reduzindo lock-in de 
+	 * recursos
+	 */
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/esl/cursospring/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/esl/cursospring/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.esl.cursospring.domain.Cliente;
 import com.esl.cursospring.domain.enums.TipoCliente;
 import com.esl.cursospring.dto.ClienteNewDTO;
+import com.esl.cursospring.repositories.ClienteRepository;
 import com.esl.cursospring.resources.exception.FieldMessage;
 import com.esl.cursospring.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +38,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
          }
          if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
         	 list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+         }
+         
+         /*
+          * Logica para testar se o email do cliente já existe
+          */
+         Cliente aux = repo.findByEmail(objDto.getEmail());
+         if(aux != null) {//se for diferente de nulo, quer dizer que encontrou um registro com esse email...
+        	 list.add(new FieldMessage("email", "Email já existe"));//...nesse caso passa a mensgaem customizada
          }
 		
 		/*
