052a26951975a9dcd0473f3ec1baa3fc6146d585
Eliederson da Silva Linhares <eliederson250@gmail.com>
2019-04-08 23:28:52 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/com/esl/cursospring/CursospringApplication.java
+++ src/main/java/com/esl/cursospring/CursospringApplication.java
@@ -1,13 +1,18 @@
 package com.esl.cursospring;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.esl.cursospring.services.S3Service;
+
 @SpringBootApplication
 public class CursospringApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -16,7 +21,7 @@ public class CursospringApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		s3Service.uploadFile("C:\\Spring\\temp\\s1.jpg");
 		
 	}
 
